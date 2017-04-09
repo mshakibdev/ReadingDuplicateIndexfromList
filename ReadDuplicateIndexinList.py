6c8c29ec7d1c4a3b77d2d0216ab0f20dec12601d
Md. Shakib Hassan <mshassan.dhk313@gmail.com>
2017-04-09 09:03:40 -0700

    Add files via upload

---
+++ ReadDuplicateIndexinList.py
@@ -0,0 +1,15 @@
+def list_duplicates_of(seq,item):
+    start_at = -1
+    locs = []
+    while True:
+        try:
+            loc = seq.index(item,start_at+1)
+        except ValueError:
+            break
+        else:
+            locs.append(loc)
+            start_at = loc
+    return locs
+
+source = [1,2,3,3,2,4,5]
+print (list_duplicates_of(source, 2))
